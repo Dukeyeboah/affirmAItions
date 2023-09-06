7bdeffa53d65f91992c11e98fa9930b11ae20e6a
Dukeyeboah <dukeyeboah@proton.me>
2023-09-06 11:58:46 -0500

    commit project

---
+++ data.js
@@ -0,0 +1,15 @@
+
+export const categoryArr = [
+  { value: "", option: "Choose a category" },
+  { value: "Housing & Home", option: "Housing & Home"},
+  { value: "Finance & Wealth", option: "Finance & Wealth" },
+  { value: "Health & Well-being", option: "Health & Well-being" },
+  { value: "Travel & Adventure", option: "Travel & Adventure"},
+  { value: "Relationships & Love", option: "Relationships & Love" },
+  { value: "Creativity & Expression", option: "Creativity & Expression" },
+  { value: "Career & Employment", option: "Career & Employment" },
+  { value: "Education & Knowledge", option: "Education & Knowledge" },
+  { value: "Spirituality & Inner Peace", option: "Spirituality & Inner Peace"},
+  { value: "Personal Growth & Development", option: "Personal Growth & Development" },
+  { value: "Self-confidence & Empowerment", option: "Self-confidence & Empowerment"},      
+]
---
+++ env.js
@@ -0,0 +1,5 @@
+export const process = {
+    env: {
+        OPENAI_API_KEY: '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'
+    }
+}
